b275711ba7026f21b7ca79776b7fff2be2271c1c
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-07-08 08:45:30 +0530

    Multipla Basic Routing Completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { About } from './about/about';
+import { A1 } from './a1/a1';
+import { A2 } from './a2/a2';
+import { A3 } from './a3/a3';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: App,
+  },
+  {
+    path: 'about',
+    component: About,
+    children: [
+      { path: '', redirectTo: 'a1', pathMatch: 'full' },
+      { path: 'a1', component: A1 },
+      { path: 'a2', component: A2 },
+      { path: 'a3', component: A3 },
+    ],
+  },
+  {
+    path: 'contact',
+    component: Contact,
+  },
+];
